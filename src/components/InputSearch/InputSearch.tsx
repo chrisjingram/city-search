daace967b24643b7abf7a806adb2dedb399a8a0b
Chris Ingram <chris@chrisingram.me>
2020-03-26 11:31:47 +0000

    Set up initial environment

---
+++ src/components/InputSearch/InputSearch.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import styles from "./InputSearch.module.css";
+
+type State = {
+
+}
+
+type Props = {
+
+}
+
+const InputSearch: React.FC<State> = (props: Props) => {
+  return (
+    <div className={styles.inputSearch}>
+
+    </div>
+  )
+};
+
+export default InputSearch;
