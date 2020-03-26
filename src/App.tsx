444283cf19863167abe606221024b4143c947324
Chris Ingram <chris@chrisingram.me>
2020-03-26 15:10:35 +0000

    Validated input

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import InputSearch from "./components/InputSearch";
 function App() {
   return (
     <div className="App">
-      <InputSearch searchClicked={searchText => console.log(searchText)}/>
+      <InputSearch search={searchText => console.log(searchText)}/>
     </div>
   );
 }
--- src/components/InputSearch/InputSearch.tsx
+++ src/components/InputSearch/InputSearch.tsx
@@ -3,15 +3,21 @@ import React from "react";
 import styles from "./InputSearch.module.scss";
 
 type Props = {
-  searchClicked(searchText:String): void
+  search(searchText:String): void
 }
 
 const InputSearch: React.FC<Props> = props => {
   const [searchText, setSearchText] = React.useState("");
+  const searchClicked = () => {
+    if(!/^[a-z]+$/i.test(searchText)){
+      return alert("Please enter the valid input.")
+    }
+    props.search(searchText);
+  };
   return (
     <div className={styles.InputSearch}>
       <input type="text" placeholder="Search for a city..." onChange={e => setSearchText(e.target.value)} value={searchText} />
-      <button className={styles.searchButton} onClick={() => props.searchClicked(searchText)}>Search</button>
+      <button className={styles.searchButton} onClick={searchClicked}>Search</button>
     </div>
   )
 };
